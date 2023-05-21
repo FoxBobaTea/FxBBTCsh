24d70aad721b4fe5a1654d386a9fa73ccd7cdba8
Steve Juarez <109123469+FoxBobaTea@users.noreply.github.com>
2023-05-20 23:29:40 -0700

    Create Ex1.cs
    Added code

---
+++ Basic Exercises/Ex1/Ex1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Ex1_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi friend, welcome to FoxBobaTea Labs");
+            /* not an actual lab, but maybe in the future there will be one */
+        }
+    }
+}
