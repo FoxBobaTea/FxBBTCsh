3a46ccc42aa5b13f72458b5f4cca307666cd029a
Steve Juarez <109123469+FoxBobaTea@users.noreply.github.com>
2023-05-21 20:44:27 -0700

    Update and rename Basic Operations to Basic Operations.cs
    added code for basic operations (sum, substraction, division and multiplication)

---
+++ Basic Exercises/Ex2/Basic Operations.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 10;
+      int y = 2;
+      
+      Console.WriteLine("'x' equals 10 and 'y' is 2");
+      Console.WriteLine(x+y);
+      Console.WriteLine(x*y);
+      Console.WriteLine(x/y);
+      Console.WriteLine(x-y);
+    }
+  }
+}
